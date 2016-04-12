515fb9318a41fb67ccd77e7d231108f4d5bb72b5
Steve Meisner <meisners@ainfosec.com>
2016-04-12 17:55:26 -0400

    1 liner to sort DHCP leases
    Signed-off-by: Steve Meisner <meisners@ainfosec.com>

--- dd-wrt-sort-leases.sh
+++ dd-wrt-sort-leases.sh
@@ -1,5 +1,1 @@
-#!/bin/sh
-data=`nvram get static_leases | awk 'BEGIN { FS="="; RS=" " } {a=1; while ( a<NF ) { b=a+1; c=a+2; d=a+3; print $c "=" $b "=" $a "=" $d; a=+4; } }' | sort | awk 'BEGIN { FS="="; RS="\n" } { a=1; while ( a<NF ) { b=a+1; c=a+2; d=a+3; printf $c "=" $b "=" $a "=" $d " "; a+=4 } }' | awk '{sub(/[ \t]+$/, "")};1'`
-nvram set static_leases="${data}"
-nvram commit
-
+nvram set static_leases="`nvram get static_leases | sed 's/\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)/.\1./g' | tr ' ' '\n' | sort -n -k2,2n -k3,3n -k4,4n -k5,5n -t. | tr '\n' ' ' | sed 's/\.\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\./\1/g' | sed 's/^ //'; echo`"
